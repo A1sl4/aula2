f3210d3a71b9d287fb49d19ff1f7c568a2c0c304
Aisla <severoaisla@gmail.com>
2025-05-30 16:17:17 -0300

    Create media.py

---
+++ media.py
@@ -0,0 +1,7 @@
+nome = str(input("Digite seu nome "))
+nota1 = int(input("Digite a primeira nota "))
+nota2 = int(input("Digite a segunda nota "))
+ 
+media = (nota1 + nota2) / 2
+
+print(f"A media da(o) aluna(o) {nome} , com a primeira nota {nota1} e a segunda nota {nota2}, foi de {media}.")
