864a830312ba55fc17877d02401ec6738b95e525
Aisla <severoaisla@gmail.com>
2025-05-30 16:07:10 -0300

    Create dirigir.py

---
+++ dirigir.py
@@ -0,0 +1,8 @@
+idade = str(input("Digite a sua idade "))
+carteira = str(input("Você possui carteira de motorista? "))
+ 
+if idade >= "18" and carteira == "sim":
+    print("Você pode dirigir!")
+else:
+    print("Você não pode dirigir :( ")
+    
