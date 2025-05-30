f03b6b24cfe931d2b365c06c5bef6620442d5307
Aisla <severoaisla@gmail.com>
2025-05-30 16:09:44 -0300

    Create login.py

---
+++ login.py
@@ -0,0 +1,7 @@
+usuario = str(input("Digite o usuario "))
+senha = str(input("Digite a senha "))
+ 
+if usuario == "tenhomedodopippi" and senha == "senha":
+    print("bem vinda(o)")
+else:
+    print("A senha ou/e usuario esta incorreto")
